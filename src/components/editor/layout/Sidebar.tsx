11df5e8e187e4842ca35f7fb7ac3b656a011dcac
thangngo <thang.ngo@sotatek.com>
2024-10-01 16:35:33 +0700

    refactor: sidebar-toggle

--- src/components/editor/layout/Sidebar.tsx
+++ src/components/editor/layout/Sidebar.tsx
@@ -12,6 +12,8 @@ import Summary from "../Summary";
 import ManualEdit from "../ManualEdit";
 import Quiz from "../Quiz";
 import { Toggle } from "@/components/ui/toggle";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Button } from "@/components/ui/button";
 
 export default function Sidebar() {
   const funcComponent: Record<SelectFunc, ReactElement> = {
@@ -21,31 +23,49 @@ export default function Sidebar() {
     [SelectFunc.EDIT]: <ManualEdit />,
   };
   const [isSelect, setIsSelect] = useState<SelectFunc>(SelectFunc.CHATBOT);
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
+
   const handleSelect = (selectFunc: SelectFunc) => {
-    setIsSelect(selectFunc);
-    setOpen(!open);
+    if (isSelect === selectFunc) {
+      setOpen(!open);
+    } else {
+      setIsSelect(selectFunc);
+      setOpen(true);
+    }
   };
   return (
     <div className="border-r flex flex-row">
       <div className=" flex flex-col p-2 space-y-5 border">
-        <Toggle
-          aria-label="Toggle bold"
+        <Button
+          variant="ghost"
+          size="icon"
           onClick={() => handleSelect(SelectFunc.CHATBOT)}
         >
-          <MessageCircleMore />
-        </Toggle>
-        <Toggle onClick={() => handleSelect(SelectFunc.SUMMARY)}>
-          <ClipboardList />
-        </Toggle>
-        <Toggle onClick={() => handleSelect(SelectFunc.QUIZ)}>
-          <PuzzleIcon />
-        </Toggle>
-        <Toggle onClick={() => handleSelect(SelectFunc.EDIT)}>
-          <PenSquare />
-        </Toggle>
+          <MessageCircleMore className="text-blue-500" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => handleSelect(SelectFunc.SUMMARY)}
+        >
+          <ClipboardList className="text-blue-500" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => handleSelect(SelectFunc.QUIZ)}
+        >
+          <PuzzleIcon className="text-blue-500" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => handleSelect(SelectFunc.EDIT)}
+        >
+          <PenSquare className="text-blue-500" />
+        </Button>
       </div>
-      {open && (
+      {open && isSelect && (
         <div className="flex flex-col w-[25rem] p-3 space-y-2">
           {funcComponent[isSelect]}
         </div>
