13edf45c931bd4ecc9eeb6a36f41524cacba850f
LiusDev <quydx.work@gmail.com>
2024-10-21 02:50:47 +0700

    feat: auth store

---
+++ src/store/auth-store.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+
+interface AuthStore {
+    token: string
+    setToken: (token: string) => void
+    removeToken: () => void
+}
+
+const useAuthStore = create<AuthStore, [["zustand/persist", AuthStore]]>(
+    persist(
+        (set) => ({
+            token: "",
+            setToken: (token) => set(() => ({ token })),
+            removeToken: () => set(() => ({ token: "" })),
+        }),
+        {
+            name: "access_token",
+        }
+    )
+)
+
+export const useAuth = () => useAuthStore((state) => state)
